3830dfb98d98eda92bba6bbe56982ef6a9d20dc3
Emanuel <jramirezg3@miumg.edu.gt>
2023-05-16 15:09:43 -0600

    Commit Incial GiftExpertApp

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,47 @@
+import {useState} from 'react'
+import { AddCategory, GifGrid } from './components'
+
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState([ 'Marvel' ]);
+    
+    const onAddCategory = (newCategory) => {
+
+        if ( categories.includes(newCategory) )return;
+
+        setCategories([newCategory,...categories]);
+    }
+
+    return (
+    <>
+        {/* titulo */}
+        <h1>Gift Expert App</h1>
+
+        {/* Input */}
+        <AddCategory 
+            // setCategories={ setCategories }
+            onNewCategory= {(value) => onAddCategory(value) }
+        />
+
+        {/* Listado de Gif */}
+        {/* el map nos sirve para presentar de manera grafica en el <li></li> las categorias */}
+        
+        { 
+            categories.map(category => (
+                <GifGrid
+                    key={ category } 
+                    category={ category } />
+            ))
+        }
+
+
+
+  
+        {/* Gift Item */}
+        
+
+
+    </>
+  )
+}
